94e738fc2547be972947abff956dfb770682fc50
Gal Noy <gal.noy@moonactive.com>
2019-02-12 16:15:53 +0200

    added slack

---
+++ libs/slack/index.js
@@ -0,0 +1,21 @@
+const { RTMClient } = require('@slack/client');
+
+// An access token (from your Slack app or custom integration - usually xoxb)
+const token = process.env.SLACK_TOKEN;
+
+// The client is initialized and then started to get an active connection to the platform
+const rtm = new RTMClient(token);
+rtm.start();
+
+rtm.on('message', (message) => {
+  // For structure of `message`, see https://api.slack.com/events/message
+
+  // Skip messages that are from a bot or my own user ID
+  if ( (message.subtype && message.subtype === 'bot_message') ||
+       (!message.subtype && message.user === rtm.activeUserId) ) {
+    return;
+  }
+
+  // Log the message
+  console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+});
